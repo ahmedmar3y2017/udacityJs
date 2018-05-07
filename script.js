b9efd5134eb42fa4108e6701008608f45966729c
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-05-07 23:55:44 +0200

    ini

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+var person={
+    name:"ahmed",
+    age:20 ,
+    address:["tanta" ,"shebin"]
+    ,
+    call:function () {
+
+        return "Done ya man Call";
+
+    }
+
+
+
+};
+
+
+console.log("This is Name : " + person.name);
+
+
